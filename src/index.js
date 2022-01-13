f6d7233adbc7785d9578e7abe1f0f78f5a8bdcdd
seb-rob <sebastianrobi29@gmail.com>
2022-01-13 09:50:41 +0530

    Hello People

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Greet = () => {
+  return <h1>Hello People</h1>
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reactDom.render(
+  <Greet />,
+  document.getElementById("root")
+)
