2d810d4ff5f38560bf206caba24677758e566a8b
seb-rob <sebastianrobi29@gmail.com>
2022-01-13 10:25:22 +0530

    iteration through peoplesData.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import reactDom from "react-dom";
 
+//Resource import
+import {peoplesData} from "./peoplesData";
+
 //component import
 import PersonComponent from "./components/Person";
 
@@ -7,7 +10,11 @@ import PersonComponent from "./components/Person";
 const PersonList = () => {
   return(
     <section>
-      <PersonComponent />
+      {peoplesData.map(people => {
+        return(
+          <PersonComponent key={people.id} {...people} ></PersonComponent>
+        )
+      })}
     </section>
   )
 }
